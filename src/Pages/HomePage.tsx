d122c01f60af06109af98e7e33723d90d4bf044b
liamengelin <liamengelin@outlook.com>
2026-02-17 03:12:48 +0100

    First Commit with broken project already

---
+++ src/Pages/HomePage.tsx
@@ -0,0 +1,14 @@
+export const HomePage = () => {
+
+    return (
+        <>
+        <h1>Hey you aren't logged in!</h1>
+        <h3>Log in below</h3>
+        <div className="card">
+        </div>
+        <p className="read-the-docs">
+            Click on the Vite and React logos to learn more
+        </p>
+        </>
+    )
+}
